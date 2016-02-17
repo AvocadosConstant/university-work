0d442dcb5bd40b27e647a1add7ceccdd55b15873
Tim <thung1@binghamton.edu>
2016-02-16 23:12:28 -0500

    Finish lab2

--- cs350/labs/Lab2Hung_thung1/lab2.c
+++ cs350/labs/Lab2Hung_thung1/lab2.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/time.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <getopt.h>
 #include <time.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #include "lab2.h"
-
-int intcmp(const void * a, const void *b) {
-    return ( *(int*)a - *(int*)b );
-}
 
 void print_usage_string(void) {
     char c;
@@ -22,48 +18,16 @@ void print_usage_string(void) {
         printf("%c",c);   
     }
     fclose(file);
-}
-
-void child_process(int level, int num_children) {
-        for(int i = 0; i < level; i++) fprintf(stderr, "\t");
-        fprintf(stderr, "Level %d Child process created.\n", level);
-        pid_t pid; /* for a child process */
-        if(level > 0) {
-            for(int i = 0; i < num_children; i++) {
-                pid = fork();
-                if(pid < 0) { /* error */
-                    fprintf(stderr, "Fork failed.");
-                    return;
-                }
-                if(pid == 0) { /* child process */
-                    child_process(level - 1, num_children);
-                }
-                else { /* parent process */
-                    parent_process(level, num_children);
-                }
-            }
-        }
-        else {  //  Leaf node
-            
-        }
-        //for(int i = 0; i < level; i++) fprintf(stderr, "\t");
-        execl("/bin/bash","c","echo \"\"",NULL);
-}
-
-void parent_process(int level, int num_children) {
-        /* parent will wait for the child to complete */
-        //fprintf(stderr, "Level %d Parent process created.\n", level);
-        wait(NULL);
-        //printf("Child Complete.\n");
-        //fprintf(stderr, "Level %d Parent process completes.\n", level);
+    exit(0);
 }
 
 int main(int argc, char **argv) {
-    
+
     static int num_levels = 0;      //  [0,4]
     static int num_children = 1;    //  [1,3]
     static int sleep_time = 1;      //  Time in seconds
-    bool ps_check = false;
+    bool ps_check = false;          //  True if both -p and -s flags were specified
+    bool will_pause = false;        //  True if -p flag was specified
 
     extern char *optarg;
     int opt = 0;
@@ -71,54 +35,81 @@ int main(int argc, char **argv) {
 		switch(opt) {
 			case 'u':{      //  prints the usage string
                 print_usage_string();
-                return 0;
             }
 			case 'N':
                 if((num_levels = atoi(optarg)) < 0 || num_levels > 4) {
-                    fprintf(stderr, "Number of levels out of bounds.\n");   
-                    return 0;
+                    fprintf(stderr, "Number of levels (%d) out of bounds.\n", num_levels);   
+                    print_usage_string();
                 }
 				break;
 			case 'M':
                 if((num_children = atoi(optarg)) < 1 || num_children > 3) {
                     fprintf(stderr, "Number of children out of bounds.\n");   
-                    return 0;
+                    print_usage_string();
                 }
 				break;
 			case 'p':
-                if(ps_check) fprintf(stderr, "Cannot specify both -p and -s flags.\n");
+                if(ps_check) {
+                    fprintf(stderr, "Cannot specify both -p and -s flags.\n");
+                    print_usage_string();
+                }
                 ps_check = true;
+                will_pause = true;
 				break;
 			case 's':
-                if(ps_check) fprintf(stderr, "Cannot specify both -p and -s flags.\n");
+                if(ps_check) {
+                    fprintf(stderr, "Cannot specify both -p and -s flags.\n");
+                    print_usage_string();
+                }
                 ps_check = true;
                 if((sleep_time = atoi(optarg)) < 0) {
                     fprintf(stderr, "Sleep time cannot be negative.\n");
+                    print_usage_string();
                 }
 				break;
 			default:
                 print_usage_string();
-				return 0;		
 		}
 	}
 
-    if(num_levels == 0) {
+    num_levels--;   //  The root node of an n-level tree will be at level n-1, so num_levels must be decremented
+
+    if(num_levels <= 0) {   //  Leaf node
+        if(num_levels == 1) num_levels--;
+        fprintf(stdout, "ALIVE:\t\tLevel 0 process with pid=%d, child of ppid=%d.\n", getpid(), getppid());
+        if(will_pause) pause();
+        else {
+            sleep(sleep_time);
+        }
+        fprintf(stdout, "EXITING:\tLevel 0 process with pid=%d, child of ppid=%d.\n", getpid(), getppid());
         return 0;
     }
 
-    pid_t pid; /* for a child process */
-    pid = fork();
+    fprintf(stdout, "ALIVE:\t\tLevel %d process with pid=%d, child of ppid=%d.\n", num_levels, getpid(), getppid());
 
-    if(pid < 0) { /* error */
-        fprintf(stderr, "Fork failed.");
-        return 1;
+    pid_t pid; /* for a child process */
+    for(int i = 0; i < num_children; i++) {
+        pid = fork();
+        if(pid < 0) { /* error */
+            fprintf(stderr, "Fork failed.");
+            return 0;
+        }
+        if(pid == 0) { /* child process */
+            char n_str[sizeof(int)];
+            sprintf(n_str, "%d", num_levels);
+            char m_str[sizeof(int)];
+            sprintf(m_str, "%d", num_children);
+            char s_str[sizeof(int)];
+            sprintf(s_str, "%d", sleep_time);
+
+            if(will_pause) execv("./lab2", (char *[]) {"./lab2", "-N", n_str, "-M", m_str, "-p", NULL});
+            else execv("./lab2", (char *[]) {"./lab2", "-N", n_str, "-M", m_str, "-s", s_str, NULL});
+        }
+        else { /* parent process */
+            wait(NULL);
+        }
     }
-    if(pid == 0) { /* child process */
-        child_process(num_levels - 1, num_children);
-    }
-    else { /* parent process */
-        parent_process(num_levels, num_children);
-    }
+    fprintf(stdout, "EXITING:\tLevel %d process with pid=%d, child of ppid=%d.\n", num_levels, getpid(), getppid());
     return 0;
 }
 
