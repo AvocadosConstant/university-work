c7fbd8f637e415e51282b0ab396c6810ca1f486e
Tim <thung1@binghamton.edu>
2016-04-18 14:23:36 -0500

    Update cs350 lab 6 and upload algo lectures

--- cs350/labs/Lab6Hung_thung1/lab6.c
+++ cs350/labs/Lab6Hung_thung1/lab6.c
@@ -4,43 +4,78 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-#define NUM_THREADS 50
 float market_value;
+int NUM_THREADS;
+int MARKET_THRESHOLD;
 pthread_mutex_t mutexsum;
 
-void *stock(void *threadid)
-{
-    long tid;
-    tid = (long)threadid;
-    //printf("Hello World! It's me, thread #%ld!\n", tid);
-
+void *stock() {
     float price = 100;
-
-    for(int i = 0; i < 1000; i++) {
+    while(true) {
         float fluc = (2 * (float)rand()/(float)RAND_MAX) - 1;
         price += fluc;
         pthread_mutex_lock (&mutexsum);
         market_value += fluc;
         pthread_mutex_unlock (&mutexsum);
-        printf("Stock %ld:\tStock fluctuation: % .2f\tStock Price: $% 3.2f\tMarket Value: $% 3.2f\n", tid, fluc, price, market_value);
-        sleep(1);
+    }
+    pthread_exit(NULL);
+}
+
+void *down_watcher() {
+    while(true) {
+        pthread_mutex_lock (&mutexsum);
+        if(market_value < NUM_THREADS * (100-MARKET_THRESHOLD)) {
+            printf("Market down to $%3.2f\n", market_value);
+            exit(0);
+        }
+        pthread_mutex_unlock (&mutexsum);
+    }
+    pthread_exit(NULL);
+}
+
+void *up_watcher() {
+    while(true) {
+        pthread_mutex_lock (&mutexsum);
+        if(market_value > NUM_THREADS * (100+MARKET_THRESHOLD)) {
+            printf("Market up to $%3.2f\n", market_value);
+            exit(0);
+        }
+        pthread_mutex_unlock (&mutexsum);
     }
     pthread_exit(NULL);
 }
 
 int main (int argc, char *argv[])
 {
-    pthread_t threads[NUM_THREADS];
+    if(argc != 3) exit(0);
+
+    NUM_THREADS = atoi(argv[1]);
+    MARKET_THRESHOLD = atoi(argv[2]);
+    pthread_t stocks[NUM_THREADS];
+    pthread_t watchers[2];
     int rc;
-    
+    time_t t;
+    srand((unsigned) time(&t));
+
     market_value = NUM_THREADS * 100;
     printf("Initial market value: $%.2f\n", market_value);
     
     //  Create two watcher threads
+    rc = pthread_create(&watchers[0], NULL, up_watcher, NULL);
+    if (rc){
+        printf("ERROR; return code from pthread_create() is %d\n", rc);
+        exit(-1);
+    }
 
-    for(long t=0; t<NUM_THREADS; t++){
-        printf("In main: creating thread %ld\n", t);
-        rc = pthread_create(&threads[t], NULL, stock, (void *)t);
+    rc = pthread_create(&watchers[1], NULL, down_watcher, NULL);
+    if (rc){
+        printf("ERROR; return code from pthread_create() is %d\n", rc);
+        exit(-1);
+    }
+
+    for(long t=2; t<NUM_THREADS; t++){
+        //printf("In main: creating stock thread %ld\n", (t - 2));
+        rc = pthread_create(&stocks[t], NULL, stock, (void *)t);
         if (rc){
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
