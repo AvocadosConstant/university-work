9c03251e654aa6e91bfee8dc969a57d801bc000b
Tim <thung1@binghamton.edu>
2016-09-21 00:19:05 -0400

    Initiate CS455

---
+++ cs455/dev/include/interface.hpp
@@ -0,0 +1,8 @@
+#ifndef _INTERFACE_H
+#define _INTERFACE_H
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+#endif
---
+++ cs455/dev/src/interface.cpp
@@ -0,0 +1,9 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+#include "interface.hpp"
+
+int main(int argv, char **argv) {
+    printf("Hello world.");
+}
