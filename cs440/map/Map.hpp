48f3016ae565e3c43f64836b99ff2aea1da6bfd8
Tim <thung1@binghamton.edu>
2017-03-04 15:30:08 -0500

    Create skeletons for map and skiplist

---
+++ cs440/map/Map.hpp
@@ -0,0 +1,9 @@
+#ifndef _MAP_HPP_
+#define _MAP_HPP_
+
+namespace cs540 {
+  template <typename Key_T, typename Mapped_T> class Map {
+
+  }
+}
+#endif
---
+++ cs440/map/SkipList.hpp
@@ -0,0 +1,8 @@
+#ifndef _SKIPLIST_HPP_
+#define _SKIPLIST_HPP_
+
+namespace cs540 {
+  template <typename Key_T, typename Mapped_T> class SkipList {
+    
+  }
+}
