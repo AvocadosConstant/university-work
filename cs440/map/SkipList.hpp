2fa5c53ff69c4ab23782c0b37f4eea28971e83d9
Tim <thung1@binghamton.edu>
2017-03-04 20:37:10 -0500

    Add some skeleton code for Skiplist (Only works as a LL atm)

--- cs440/map/SkipList.hpp
+++ cs440/map/SkipList.hpp
@@ -1,9 +1,58 @@
 #ifndef _SKIPLIST_HPP_
 #define _SKIPLIST_HPP_
 
+#include <iostream>
+
 namespace cs540 {
   template <typename Key_T, typename Mapped_T> class SkipList {
-    
+    typedef std::pair<Key_T, Mapped_T> ValueType;
+
+    struct SNode {
+      SNode *next, *prev;
+    };
+
+    struct Node : public SNode {
+      ValueType data;
+
+      Node() : SNode() {}
+      Node(Key_T key, Mapped_T value) : data{key, value} {}
+      Node(ValueType data) : data{data} {}
+    };
+
+    public:
+      SNode *head;
+
+      SkipList() {
+        head = new SNode();
+        head->next = head;
+        head->prev = head;
+        std::cout << "Constructed" << std::endl;
+      }
+
+      // TODO Replace return type with std::pair<Iterator, bool> 
+      void insert(const ValueType &element) {
+        Node *tmp = new Node(element);
+        tmp->next = head;
+        head->prev->next = tmp;
+        tmp->prev = head->prev;
+        head->prev = tmp;
+      }
+
+      /*
+      Iterator find(const Key_T &);
+
+      ConstIterator find(const Key_T &) const;
+
+      Mapped_T &at(const Key_T &) const;
+
+      const Mapped_T &at(const Key_T &) const;
+      */
+
+      void print() {
+        for (SNode *p = head->next; p != head; p = p->next) {
+          std::cout << ((Node*)p)->data.second << std::endl;
+        }
+      }
   };
 }
 
--- cs440/map/test.cpp
+++ cs440/map/test.cpp
@@ -1,9 +1,15 @@
 #include "Map.hpp"
 
 #include <iostream>
-#include <cassert>
+#include <assert.h>
 
 int main() {
   cs540::Map<int, int> int_int_map;
+
+  cs540::SkipList<int, int> skip;
+  for(int i = 0; i < 10; i++) {
+    skip.insert({i, i});
+  }
+  skip.print();
   return 0;
 }
