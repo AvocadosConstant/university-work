fa8495013f4c1a0a574bb543567f183ba4b27a9b
Tim <thung1@binghamton.edu>
2017-05-16 09:15:55 -0400

    Create Array.hpp and import a Makefile

---
+++ cs440/multi-dim-array/Array.hpp
@@ -0,0 +1,11 @@
+#ifndef _ARRAY_HPP_
+#define _ARRAY_HPP_
+
+#include <iostream>
+#include <stdlib.h>
+
+namespace cs540 {
+  template <typename T, std::size_t... Dims> class Array {
+  }; // class Array
+} // namespace cs540
+#endif
