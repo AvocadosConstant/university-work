66faf03273c9ddd000a3699a8fb8fda01130a9f5
Tim Hung <thung1@binghamton.edu>
2017-10-15 18:47:17 -0700

    Implement model with 96% accuracy

--- cs436/hw/hw2/tim_hung_hw2/classytext.py
+++ cs436/hw/hw2/tim_hung_hw2/classytext.py
@@ -1,43 +1,96 @@
-import os, string
+import os, string, re
+from math import log
+from collections import Counter
+
 
 class Document:
     def __init__(self, filename):
         self.filename = filename
-        self.remove = dict.fromkeys(
-                map(ord, '\n' + string.punctuation))
-
-    def gen_words(self):
         with open(self.filename, 'rb+') as f:
-            for line in f:
-                #print(str(line).strip().lower().split())
-                for word in str(line).strip().lower().translate(
-                    self.remove).split():
-                    yield word
+            # extract all words
+            self.words = re.findall(r'\w[a-z]+',
+                f.read().decode('latin-1').lower())
+
 
 class BagOfWords:
-    def __init__(self):
-        self.freqs = {}
-        self.size = 0
+    def __init__(self, label):
+        self.freqs = Counter()
         self.num_docs = 0
+        self.word_count = 0
     
+    def __len__(self):
+        return len(self.freqs)
+
     def parse(self, doc):
-        for word in doc.gen_words():
-            self.freqs[word] = self.freqs.get(word, 0) + 1
-        self.size = len(self.freqs)
+        self.freqs.update(doc.words)
         self.num_docs += 1
+        self.word_count = sum(self.freqs.values())
+
+    def remove_words(self, words):
+        for word in words:
+            if word in self.freqs:
+                del self.freqs[word]
+        self.word_count = sum(self.freqs.values())
+
 
 class NaiveBayes:
     def __init__(self):
         self.bags = {}
+        self.vocab = Counter()
+        self.num_docs = 0
+        self.stop_words = []
+
+    def sitrep(self):
+        """ Displays some helpful information about the model for testing """
+        print('\n============ Model information ============\n')
+        print(len(self.vocab), 'unique words')
+        for label, bag in self.bags.items():
+            print('\n--------\n')
+            print('For class label', label)
+            print('Trained on ', self.bags[label].num_docs, 'documents')
+            print(len(self.bags[label]), 'unique words')
+            print('10 most common words:',
+                  ', '.join([tup[0] for tup in bag.freqs.most_common(10)]))
+        print('\n===========================================\n')
+
+    def set_stop_words(self, path):
+        with open(path, 'rb+') as f:
+            self.stop_words = f.read().decode('latin-1').split()
 
     def train(self, labels_paths):
+        """ Trains model upon training data """
         for label, path in labels_paths:
-            self.bags[label] = BagOfWords()
-
+            self.bags[label] = BagOfWords(label)
             for filename in os.listdir(path):
                 doc = Document(path + filename)
                 self.bags[label].parse(doc)
 
-            print(label)
-            print(self.bags[label].num_docs, 'documents')
-            print(self.bags[label].size, 'unique words')
+            # remove stop words
+            self.bags[label].remove_words(self.stop_words)
+
+            # update vocab
+            self.vocab += self.bags[label].freqs
+
+            # update num_docs
+            self.num_docs += self.bags[label].num_docs
+
+    def classify(self, test_bag, k=1):
+        """ Returns the most probable class given a dataset
+
+        test_bag    BagOfWords of document to classify
+        k           Laplace smoothing parameter
+        """
+        best_class, best_posterior = '', -2**30
+        for label, bag in self.bags.items():
+            prior = log(bag.num_docs / self.num_docs)
+            denominator = bag.word_count + k * len(self.vocab)
+            likelihood = 0
+            for word, count in test_bag.freqs.items():
+                if word not in self.stop_words:
+                    numerator = bag.freqs.get(word, 0) + k
+                    likelihood += count * log(numerator / denominator)
+            posterior = prior + likelihood
+            if posterior > best_posterior:
+                best_posterior = posterior
+                best_class = label
+        return best_class
--- cs436/hw/hw2/tim_hung_hw2/hw2.py
+++ cs436/hw/hw2/tim_hung_hw2/hw2.py
@@ -6,10 +6,8 @@
     at Binghamton University taught by Professor Arti Ramesh.
 """
 
-import os
-import string
-import argparse
-from classytext import NaiveBayes
+import os, string, argparse
+from classytext import NaiveBayes, BagOfWords, Document
 
 def train(model, path):
     labels_paths = [
@@ -17,20 +15,39 @@ def train(model, path):
         for label in ['spam', 'ham']]
     model.train(labels_paths)
 
+def classify_accuracy(model, path):
+    num_docs, num_correct = 0, 0
+    labels_paths = [
+        (label, path + label + '/')
+        for label in ['spam', 'ham']]
+    for label, path in labels_paths:
+        for filename in os.listdir(path):
+            bag = BagOfWords(label)
+            doc = Document(path + filename)
+            bag.parse(doc)
+            if label == model.classify(bag, k=1):
+                num_correct += 1
+            num_docs += 1
+    return num_correct / num_docs
+
 def main():
     parser = argparse.ArgumentParser(
         description='Classify some text as spam or ham'\
             ' using a multinomial Naive Bayes classifier.')
-
     parser.add_argument('stop_dir',  help='Directory containing stopWords.txt')
     parser.add_argument('train_dir', help='Directory containing training data')
     parser.add_argument('test_dir',  help='Directory containing testing data')
-
     args = vars(parser.parse_args())
 
     model = NaiveBayes()
+    model.set_stop_words(args['stop_dir'] + 'stopWords.txt')
+
     train(model, args['train_dir'])
 
+    model.sitrep()
+
+    print('Model Accuracy:',
+          classify_accuracy(model, args['test_dir']))
 
 if __name__ == '__main__':
     main()
